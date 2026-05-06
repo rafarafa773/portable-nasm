5ab9ee63ccd6045a1ef1ac569c8cfb12decdbf04
rafarafa773 <rafael.fagundes@auxiliadora.net>
2026-05-06 01:25:32 -0300

    one of the worst interpreter ever made

---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+typedef enum {
+	MOV, /* 1 */
+	JMP, // 2
+	CMP, // 3
+	ADD,	/* 4 */
+	db,
+	error
+} ins;
+ins lor(char *cmnd) {
+	if(strcmp(cmnd, "mov") == 0) return MOV;
+	if(strcmp(cmnd, "add") == 0) return ADD;
+	if(strcmp(cmnd, "db") == 0) return db;
+	/* keep ADD and CMP handling as originally requested (do not add them) */
+	return error;
+}	
+		
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		printf("%s <filename>%c", argv[0], 10);
+		return 1;
+	}
+	/* removed the incomplete line; assign op after reading command */
+	FILE *cl = fopen(argv[1], "r");
+	char seecmd[50];
+	char value[50];
+	char name[50];
+	char randomstring[100];
+	int temp1;
+	if (cl == NULL) {
+		printf("error");
+		return -1;
+	}
+	char ch;
+	while (fscanf(cl, "%s %[^,], %s", seecmd, name, value) == 3) {
+		ins op = lor(seecmd);
+		switch(op) {
+			case db:
+				char *stringandond[50];
+				*stringandond = value;
+				printf("%s%c", *stringandond, 10);
+				break;
+			case MOV:
+				temp1 = atoi(value);
+				printf("%d%c", temp1,10);
+				break;
+			case ADD:
+				temp1 = temp1 + atoi(value);
+				printf("%d%c", temp1, 10);
+				break;
+			default:
+				printf("ixi molecada eu ainda não fiz isso%c", 10);
+				fclose(cl);
+				return -1;
+		}
+	}
+	fclose(cl);
+
+	return 0;
+}
+
